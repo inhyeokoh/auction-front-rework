52cfa224db58a91b92698cfa295030128943e326
rlawjdgkr <irlawjdgkr@naver.com>
2025-03-19 22:50:58 +0900

    Auctions css조정

--- src/pages/Auctions.jsx
+++ src/pages/Auctions.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Card, CardContent, CardTitle, CardFooter } from "../components/ui/Card";
 import Button from "../components/ui/Button";
-import "./Auctions.css";
+import "../styles/Auctions.css";
 
 const Auctions = () => {
   const navigate = useNavigate();
