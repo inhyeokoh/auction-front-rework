46b0ad3f0b6dc60178249a9ed708d4b6e0b2e1ed
tony24123 <tjdduswns01@naver.com>
2025-03-31 21:00:17 +0900

    final fix

--- src/config/host-config.js
+++ src/config/host-config.js
@@ -29,7 +29,7 @@ if (clientHostName ==='localhost') {
 // 클라이언트가 localhost면
 // http://localhost:8088
 // 클라이언트가 goose123.shop라면
-// https://43.202.28.18:8088
+// https://api.goose123.shop:8088
 // const API_BASE_URL = `${backendHostName.replace('http://', 'https://')}`;
 export const API_BASE_URL = `${backendHostName}`;
 
